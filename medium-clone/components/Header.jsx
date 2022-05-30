1524b3161c5efd91769f910e7eefa425b10f039e
Swanand-Wagh <swanandwagh7@gmail.com>
2022-05-30 10:32:44 +0530

    Header component ready

--- medium-clone/components/Header.jsx
+++ medium-clone/components/Header.jsx
@@ -1,28 +1,33 @@
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
+import styles from "../styles/Header.module.css";
 
 const Header = () => {
   return (
     <>
-      <header>
-        <div>
+      <header className={styles.header}>
+        <div className={styles.header_left}>
           <div>
             <Link href="/">
-              <img src="https://links.papareact.com/yvf" alt="medium_logo" />
+              <img
+                className={styles.logo}
+                src="https://links.papareact.com/yvf"
+                alt="medium_logo"
+              />
             </Link>
           </div>
 
-          <div>
+          <di className={styles.header_links}>
             <h3>About</h3>
             <h3>Contact</h3>
-            <h3>Follow</h3>
-          </div>
+            <h3 className={styles.follow_btn}>Follow</h3>
+          </di>
         </div>
 
-        <div>
+        <div className={styles.header_right}>
           <h3>Sign In</h3>
-          <h3>Get Started</h3>
+          <h3 className={styles.getStarted_btn}>Get Started</h3>
         </div>
       </header>
     </>
